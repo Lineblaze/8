ad6b0f490ffdcf292af0aba1332ece2fcf7a2e07
Roman Kochanov <lineblaze@mail.ru>
2020-12-23 19:16:26 +0300

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Задание 1 */
+console.log('Задача 1, Считаем овец');
+let n = prompt('Введите кол-во овец:');
+for (let i = 1; i <= n; i++) {
+    console.log(`${i} овечка...`)
+}
+console.log('\n')
+
