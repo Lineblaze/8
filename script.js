d8edc40da36bf41606a789925516b5783de576e1
Roman Kochanov <lineblaze@mail.ru>
2020-12-23 19:49:41 +0300

    4

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Задание 1 */
-console.log('Задача 1, Считаем овец');
+console.log('Задание 1, Считаем овец');
 let n = prompt('Введите кол-во овец:');
 for (let i = 1; i <= n; i++) {
     console.log(`${i} овечка...`)
@@ -7,7 +7,7 @@ for (let i = 1; i <= n; i++) {
 console.log('\n')
 
 /* Задание 2 */
-console.log('Задача 2, Четное-нечетное');
+console.log('Задание 2, Четное-нечетное');
 let i = 0;
 while(i < 16) {
     if (i % 2 == 0) {
@@ -21,7 +21,7 @@ while(i < 16) {
 console.log('\n')
 
 /* Задание 3 */
-console.log('Задача 3, Шахматная доска');
+console.log('Задание 3, Шахматная доска');
 let x = 0;
 while(x < 8) {
     if (x % 2 == 0) {
@@ -33,5 +33,33 @@ while(x < 8) {
     x++;
 }
 console.log('\n')
+
+/* Задание 4 */
+console.log('Задание 4, Сортировка');
+let num1 = -3;
+let num2 = 2;
+let num3 = 0;
+if (num1 <= num2 && num2 <= num3) {
+    console.log(num1, num2, num3)
+}
+if (num1 <= num3 && num3 <= num2) {
+    console.log(num1, num3, num2)
+}
+if (num2 <= num1 && num1 <= num3) {
+    console.log(num2, num1, num3)
+}  
+if (num2 <= num3 && num3 <= num1) {
+    console.log(num2, num3, num1)
+}
+if (num3 <= num1 && num1 <= num2) {
+    console.log(num3, num1, num2)
+}
+if (num3 <= num2 && num2 <= num1) {
+    console.log(num1, num2, num3)
+}   
+console.log('\n')
+
+
+
 
 
